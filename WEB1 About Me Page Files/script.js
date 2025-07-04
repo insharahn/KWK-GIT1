c75a6d5fc1cf0d96b20ec19e57ad2157954a6c38
insharahn <insharahnazir@gmail.com>
2025-07-04 18:40:48 +0500

    updated formatting and random fact function

--- WEB1 About Me Page Files/script.js
+++ WEB1 About Me Page Files/script.js
@@ -5,9 +5,12 @@ const facts = [
   "I am the first woman on my mother's side of the family in three direct generations to NOT become a doctor!",
   "Assembly was my favorite language to learn. I was heartbroken to find out it isn't wildly used.",
   "My family loves travelling and I've visted over 10 countries, but I hate airplanes.",
+  "I've never broken a bone.",
+  "My cat's name is Totoro. He's a gray Persian and the love of my life.",
 ];
 
 function getRandomFact() {
-  const fact = facts[Math.floor(Math.random() * facts.length)];
-  document.getElementById("funFactDisplay").textContent = fact;
+  //set text content to random index from facts array
+  document.getElementById("funFactDisplay").textContent =
+    facts[Math.floor(Math.random() * facts.length)];
 }
